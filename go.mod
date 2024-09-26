dab478b3bd52048aac1fb8efccdccea91eb29c06
Herman <hmurauyou@icloud.com>
2024-09-26 18:49:54 +0300

    Binary Tree added.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shakew3ll/data_structures_go.git
+
+go 1.21.4
---
+++ main/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shakew3ll/data_structures_go.git/binary_tree"
+	"github.com/shakew3ll/data_structures_go.git/hash_map"
+)
+
+func main() {
+	fmt.Println("################")
+	fmt.Println("# Binary Tree: #")
+	fmt.Println("################")
+	fmt.Println("")
+	binary_tree.BinaryTree()
+	fmt.Println("\n")
+
+	fmt.Println("################")
+	fmt.Println("# Hash Map: #")
+	fmt.Println("################")
+	fmt.Println("")
+	hash_map.HashMap()
+	fmt.Println("\n")
+}
