f6ac2c53921932bec7a624453306526afb4c75ac
Herman <hmurauyou@icloud.com>
2024-09-27 16:09:56 +0300

    Linter fixes -v1.1.

--- linked_list/linked_list.new.go
+++ linked_list/linked_list.new.go
@@ -13,56 +13,56 @@ func LinkedList() {
 	ll.Append(50)
 
 	ll.Traverse(func(value int) {
-		fmt.Println(value)
+		fmt.Printf("%d\n", value) // Use fmt.Printf instead of fmt.Println
 	})
 
 	found, err := ll.Search(20)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error: %v\n", err)
 	} else {
-		fmt.Println("Found:", found)
+		fmt.Printf("Found: %d\n", found)
 	}
 
 	err = ll.InsertAt(25, 2)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Printf("Error: %v\n", err)
 	}
 	fmt.Println("List after inserting 25 at position 2:")
 	ll.Traverse(func(value int) {
-		fmt.Println(value)
+		fmt.Printf("%d\n", value)
 	})
 
 	err = ll.Remove(20)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error: %v\n", err)
 	}
 	fmt.Println("List after removing 20:")
 	ll.Traverse(func(value int) {
-		fmt.Println(value)
+		fmt.Printf("%d\n", value)
 	})
 
 	err = ll.RemoveFirst()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error: %v\n", err)
 	}
 
 	err = ll.RemoveLast()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error: %v\n", err)
 	}
 
 	fmt.Println("Final list:")
 	ll.Traverse(func(value int) {
-		fmt.Println(value)
+		fmt.Printf("%d\n", value)
 	})
 
 	slice := ll.ToSlice()
-	fmt.Println("List converted to slice:", slice)
+	fmt.Printf("List converted to slice: %v\n", slice)
 
 	ll.Reverse()
 
 	fmt.Println("Reversed list:")
 	ll.Traverse(func(value int) {
-		fmt.Println(value)
+		fmt.Printf("%d\n", value)
 	})
 }
